e8b45573ca9019f6ad8290cc37da9229faeab79b
AshkanGhomizadeh <89537816+AshkanGhomizadeh@users.noreply.github.com>
2021-09-16 00:45:15 +0430

    Add files via upload

---
+++ s5e2.py
@@ -0,0 +1,6 @@
+#Ashkan Ghomizadeh Thursdays 14:00 - 18:00
+#dar yek khat 3 list ezafe shavad sepas baraks yeki dar miyan chap shavad
+numbers_1 = [1, 2, 3]
+numbers_2 = [4, 5, 6]
+numbers_3 = [7, 8, 9]
+print((numbers_1 + numbers_2 + numbers_3)[::-2])
